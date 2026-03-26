53a17f7746c4b293f76abafa98b2d40c324317cd
Tiardo Wiechmann <205292868+TiardoWiechmann@users.noreply.github.com>
2026-03-26 08:28:09 +0100

    Add function for adding book to library

--- javascript.js
+++ javascript.js
@@ -1,16 +1,29 @@
-function Book(title, author, pages, read) {
+const myLibrary = [];
+
+function Book(id, title, author, pages, read) {
     if (!new.target) {
         throw Error("You must use the 'new' operator to call the constructor");
     }
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 
     this.info = function() {
-        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`);
+        const r = this.read ? "read it" : "not read it yet";
+        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${r}`);
     }
 }
 
-const book1 = new Book("Harry Potter", "J.K. Rowling", 500, "not read.");
-book1.info();
+function addBookToLibrary(title, author, pages, read) {
+    const id = crypto.randomUUID;
+    const book = new Book(id, title, author, pages, read);
+    myLibrary.push(book);
+}
+
+/* const book1 = new Book("Harry Potter", "J.K. Rowling", 500, true);
+book1.info(); */
+addBookToLibrary("Harry Potter", "Rowling", 500, false);
+
+console.log(myLibrary);
