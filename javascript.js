f2a290ffb857082b5acfd3f761a162c521bf4957
Tiardo Wiechmann <205292868+TiardoWiechmann@users.noreply.github.com>
2026-03-26 10:53:03 +0100

    Create funcion that lists all books

--- javascript.js
+++ javascript.js
@@ -17,13 +17,62 @@ function Book(id, title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-    const id = crypto.randomUUID;
+    const id = crypto.randomUUID();
     const book = new Book(id, title, author, pages, read);
     myLibrary.push(book);
 }
 
-/* const book1 = new Book("Harry Potter", "J.K. Rowling", 500, true);
-book1.info(); */
+function displayAllBooks() {
+    const container = document.createElement("div");
+    const table = createTable(myLibrary[0]);
+    myLibrary.forEach(function (book){
+        const row = appendRow(table, book);
+        table.appendChild(row);
+    });
+
+    container.appendChild(table);
+    const body = document.querySelector("body");
+    body.prepend(container);
+}
+
+function createTable(book) {
+    const table = document.createElement("table");
+    table.style.margin = "0 auto";
+    const row = document.createElement("tr");
+    for (prop in book) {
+        if (typeof book[prop] === "function") {
+            continue;
+        }
+        const th = document.createElement("th");
+        th.textContent = prop;
+        table.appendChild(th);
+        console.log(typeof book[prop]);
+    }
+    return table;
+}
+
+function appendRow(table, book) {
+    const row = document.createElement("tr");
+    for (prop in book) {
+        if (typeof book[prop] === "function") {
+            continue;
+        }
+        const cell = document.createElement("td");
+        cell.textContent = book[prop];
+        row.appendChild(cell);
+    }
+    return row;
+}
+
+
+
+
+
+
+// const book1 = new Book("Harry Potter", "J.K. Rowling", 500, true);
+// book1.info();
 addBookToLibrary("Harry Potter", "Rowling", 500, false);
+addBookToLibrary("Harry Potter2", "Rowling", 500, true);
 
 console.log(myLibrary);
+displayAllBooks();
