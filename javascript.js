e4493b3d32ab2296c4959a9dc3ef52ea11e463a9
Tiardo Wiechmann <205292868+TiardoWiechmann@users.noreply.github.com>
2026-04-15 19:03:30 +0200

    Refactor to use class

--- javascript.js
+++ javascript.js
@@ -1,17 +1,31 @@
-function Book(id, title, author, pages, read) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor");
+class Book {
+    constructor(id, title, author, pages, read) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
     }
-    this.id = id;
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.info = function() {
-        const r = this.read ? "read it" : "not read it yet";
-        console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${r}`);
+
+    toggleRead() {
+        if (this.read.toLowerCase() === "no") {
+            this.read = "yes";
+        }
+        else {
+            this.read = "no";
+        }
     }
 }
+
+Book.prototype.toggleRead = function () {
+    if (this.read.toLowerCase() === "no") {
+        this.read = "yes";
+    }
+    else {
+        this.read = "no";
+    }
+};
+
 
 
 function addBookToLibrary(title, author, pages, read) {
@@ -123,16 +137,6 @@ function updateLibrary(book_b_id) {
     const i = myLibrary.indexOf(delBook);
     myLibrary.splice(i, 1);
 }
-
-
-Book.prototype.toggleRead = function () {
-    if (this.read.toLowerCase() === "no") {
-        this.read = "yes";
-    }
-    else {
-        this.read = "no";
-    }
-};
 
 
 const myLibrary = [];
